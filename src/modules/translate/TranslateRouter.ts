e4ef319c8740b3dda06228cd0ae54a616d0572c6
Nicolas14200 <heutte.nicolas@laposte.net>
2023-04-12 16:11:30 +0200

    second commit

--- src/modules/translate/TranslateRouter.ts
+++ src/modules/translate/TranslateRouter.ts
@@ -1,73 +1,87 @@
-import * as deepl from 'deepl-node';
-import { express,dotenv } from '../../index';
-import * as jwt from 'jsonwebtoken';
+import * as deepl from "deepl-node";
+import { express, dotenv } from "../../index";
+import * as jwt from "jsonwebtoken";
 // .ENV
 const env = dotenv.load({
-  DEEPL_KEY:String,
-  JWT_KEY:String
-})
-const deepl_authKey : string = env.DEEPL_KEY;
-const jwt_key :string = env.JWT_KEY;
-const translator = new deepl.Translator(deepl_authKey);
+  DEEPL_KEY: String,
+  JWT_KEY: String,
+});
+const deepl_authKey: string = env.DEEPL_KEY;
+const jwt_key: string = env.JWT_KEY;
+const translator: deepl.Translator = new deepl.Translator(deepl_authKey);
 
 interface SentenceStat {
-  user:string;
-  textToTranslate:string;
-  lang : string;
-  sentence : string;
+  textToTranslate: string;
+  lang: string;
+  sentence: string;
 }
-const sentenceMap : Map<string, SentenceStat> =  new Map();
+const sentenceArr:SentenceStat[] = [];
+const sentenceMap: Map<string, SentenceStat[]> = new Map();
 
-const TranslateRouter : express.Router = express.Router();
+const TranslateRouter: express.Router = express.Router();
 
-TranslateRouter.post('/',async (req: express.Request, res: express.Response)=>{
-  try{
-    const token = <string> req.headers["access_key"];
-    const decoded = <jwt.JwtPayload> jwt.verify(token,jwt_key);
-    const mailActive : string = decoded.user.email;
+//FUNCTION
+function tokenGetMail (token:string):string{
+  const decoded = <jwt.JwtPayload>jwt.verify(token, jwt_key);
+  return decoded.user.email;
+}
+TranslateRouter.post(
+  "/",
+  async (req: express.Request, res: express.Response) => {
+    try {
+      const mailActive = tokenGetMail (<string>req.headers["access_key"]);
 
-    const textToTranslate : string = req.body.text;
-    const langTo = req.body.lang; 
-
-    const targetSentence : SentenceStat|undefined = sentenceMap.get(textToTranslate);
-
-    if (!targetSentence){
-      const result = await translator.translateText(textToTranslate, 'fr', langTo);
-      const sentence : SentenceStat = {
-        user:mailActive,
-        textToTranslate:textToTranslate,
-        lang:langTo,
-        sentence : result.text
-    }
-      sentenceMap.set(textToTranslate,sentence);
+      const textToTranslate: string = req.body.text;
+      const langTo = req.body.lang;
+      
+      const targetSentence:SentenceStat[] | undefined =sentenceMap.get(mailActive);
+      if (targetSentence){
+        for(let i = 0 ; i<targetSentence.length;i++){
+          if (targetSentence[i].textToTranslate === textToTranslate) {
+            console.log(sentenceMap);
+            return res.status(200).send(targetSentence);
+          }
+        }
+      }
+      const result = await translator.translateText(
+        textToTranslate,
+        "fr",
+        langTo
+      );
+      const sentence: SentenceStat = {
+        textToTranslate: textToTranslate,
+        lang: langTo,
+        sentence: result.text,
+      };
+      sentenceArr.push(sentence)
+      sentenceMap.set(mailActive, sentenceArr);
+      console.log(sentenceMap);
       return res.status(200).send(result.text);
+
+    } catch (e) {
+      return res.status(404).send(e);
     }
-    console.log(sentenceMap);
-    return res.status(200).send(targetSentence);
-  }catch(e){
-    return res.status(404).send(e)
   }
-})
-TranslateRouter.get('/languages', async (req: express.Request, res: express.Response)=>{
-  const arrLang : string[] = [];
-  const sourceLanguages = await translator.getSourceLanguages();
-  for (let i = 0; i < sourceLanguages.length; i++) {
-    const lang = sourceLanguages[i];
-    arrLang.push(`${lang.name} (${lang.code})`);
+);
+TranslateRouter.get(
+  "/languages",
+  async (req: express.Request, res: express.Response) => {
+    const arrLang: string[] = [];
+    const sourceLanguages = await translator.getSourceLanguages();
+    for (let i = 0; i < sourceLanguages.length; i++) {
+      const lang = sourceLanguages[i];
+      arrLang.push(`${lang.name} (${lang.code})`);
+    }
+    return res.status(200).send(arrLang);
   }
-  return res.status(200).send(arrLang);
-})
-TranslateRouter.get('/', (req: express.Request, res: express.Response)=>{
-  const iter : IterableIterator<string> = sentenceMap.keys();
-  let arr :string[] = []
-  for (const value of iter) {
-      arr.push(value)
-  }
-  return res.status(200).send(arr);
-})
+);
+TranslateRouter.get("/", (req: express.Request, res: express.Response) => {
+  const mailActive: string = tokenGetMail (<string>req.headers["access_key"]);
+  console.log(mailActive);
+  console.log(sentenceMap.get(mailActive))
+  const history :  SentenceStat[]|undefined= sentenceMap.get(mailActive);
+  console.log(history);
+  return res.status(200).send(history);
+});
 
-export {TranslateRouter};
-
-
-
-
+export { TranslateRouter };
